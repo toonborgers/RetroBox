10b16728c56b0030835130492e7b4fa0e7a23e2b
Toon Borgers <borgers.toon@gmail.com>
2014-01-24 08:46:39 +0100

    Speed up build

---
+++ Android/retrobox/src/instrumentTest/java/be/cegeka/retrobox/Test.java
@@ -0,0 +1,12 @@
+package be.cegeka.retrobox;
+
+import android.test.AndroidTestCase;
+
+import org.fest.assertions.api.Assertions;
+
+public class Test extends AndroidTestCase {
+
+    public void testDummy() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
